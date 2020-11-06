489c0a5835c98daf3370acabaa5d73347d22b265
Paranoiya228 <72651517+Paranoiya228@users.noreply.github.com>
2020-11-07 01:18:54 +0300

    prrr

---
+++ academy/src/by/academy/practice/arrays/FirstCreateArray.java
@@ -0,0 +1,22 @@
+package by.academy.practice.arrays;
+
+public class FirstCreateArray {
+
+	public static void main(String[] args) {
+		int[] arr = {2,4,6,8,10,12,14,16,18,20};
+		
+		for(int i:arr)
+		{
+			System.out.print(i+" ");
+		}
+		
+		System.out.println();
+		
+		for(int i:arr)
+		{
+			System.out.println(i);
+		}
+
+	}
+
+}
---
+++ academy/src/by/academy/practice/arrays/ThirdRandArray.java
@@ -0,0 +1,28 @@
+package by.academy.practice.arrays;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class ThirdRandArray {
+	public static void main(String[] args)
+	{
+		int min =0, max=9, dif=max-min, i, sum=0;
+		
+		int[] arr = new int[15];
+		Random rand = new Random();
+		for (i = 0; i < arr.length; i++) 
+			arr[i] = rand.nextInt(dif+min);
+		
+		System.out.println(Arrays.toString(arr));
+		
+		for(i=0;i < arr.length;i++)
+		{
+			if(arr[i]%2==0)
+				sum++;	
+		}
+		
+		System.out.println(sum);
+
+	}
+
+}
