bbc09c08c77c72f6bfdb64c485453ad633864c9f
Paranoiya228 <72651517+Paranoiya228@users.noreply.github.com>
2020-11-07 01:06:30 +0300

    practice

---
+++ academy/src/by/academy/practice/SecondNecheynue.java
@@ -0,0 +1,13 @@
+package by.academy.practice;
+
+public class SecondNecheynue {
+	public static void main(String[] args)
+	{
+		int[] arr = new int[50];
+		for(int i=0, j=1;i<arr.length;i++,j+=2) 
+			arr[i]=j;
+		for(int i=arr.length-1;i>=0;i--) 
+			System.out.print(arr[i]+" ");
+	}
+
+}
