e7fcd8ffc3c7c782fa340f7617f5c30479ef5b88
Paranoiya228 <72651517+Paranoiya228@users.noreply.github.com>
2020-11-30 23:06:34 +0300

    hh

--- academy/src/by/academy/homework/homework4/Date/DateDemo.java
+++ academy/src/by/academy/homework/homework4/Date/DateDemo.java
@@ -3,16 +3,18 @@ package by.academy.homework.homework4.Date;
 import java.text.ParseException;
 
 public class DateDemo {
-	
+
 	public static void main(String[] argc) throws ParseException {
-	
-	Datte date = new Datte();
-	date = date.createDate();
-	
-	date.DayOfWeek();
-	
-	
-	
-	
+
+		Datte date = new Datte();
+		date = date.createDate();
+
+		date.DayOfWeek();
+		
+		String str ="25-02-2001";
+		
+		Datte date1 = new Datte(str);
+		
+		date1.isValid();
 	}
 }
--- academy/src/by/academy/homework/homework4/Date/Datte.java
+++ academy/src/by/academy/homework/homework4/Date/Datte.java
@@ -4,12 +4,15 @@ import java.text.ParseException;
 import java.time.LocalDate;
 import java.time.DayOfWeek;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Datte {
 
 	private int day;
 	private int month;
 	private int year;
+	String date;
 
 	public int getDay() {
 		return day;
@@ -35,6 +38,14 @@ public class Datte {
 		this.year = year;
 	}
 
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
 	public Datte() {
 		super();
 	}
@@ -45,11 +56,18 @@ public class Datte {
 		this.month = month;
 		this.year = year;
 	}
-	
+
+	public Datte(String date) {
+		super();
+		this.date = date;
+	}
+
+
+
 	public static class Day {
 
 	}
-	
+
 	public static class Month {
 
 	}
@@ -71,6 +89,15 @@ public class Datte {
 		}
 
 	}
+	
+	public void isValid() {
+
+		String strPattern = "^[0-9]{2}-[0-9]{2}-[0-9]{4}$";
+		Pattern pattern = Pattern.compile(strPattern);
+		Matcher matcher = pattern.matcher(getDate());
+
+		System.out.println("The date is " + (matcher.matches() == true ? "valid" : "invalid"));
+	}
 
 	public Datte createDate() {
 
@@ -87,20 +114,19 @@ public class Datte {
 		return date;
 
 	}
-	
+
 	public void DayOfWeek() throws ParseException {
-		
-		
-	     int year = getYear();
-	     int month = getMonth();
-	     int day = getDay();
-	     
-	     LocalDate date = LocalDate.of(year,month,day); 
-	     
-	     DayOfWeek dayOfWeek = date.getDayOfWeek();
-	     
-	     System.out.println(date);
-	     System.out.println(dayOfWeek);
+
+		int year = getYear();
+		int month = getMonth();
+		int day = getDay();
+
+		LocalDate date = LocalDate.of(year, month, day);
+
+		DayOfWeek dayOfWeek = date.getDayOfWeek();
+
+		System.out.println(date);
+		System.out.println(dayOfWeek);
 	}
 
 	public int inputInt() {
